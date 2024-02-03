70fb814347c903645977ac07d995c00ba42c2133
Z1gSt1K <mp910265@gmail.com>
2024-02-03 16:22:17 +0300

    first

---
+++ main.py
@@ -0,0 +1,17 @@
+f=open('game.txt').read()
+a=[]
+fs= f.split('\n')
+for i in fs:
+    a.append(i.split(sep='$'))
+
+for i in a:
+    if '55' in i[2]:
+        characters = str(i[1])
+        GameName=str(i[0])
+        nameError=str(i[2])
+        date=str(i[3])
+        print('У персонажа '+ characters+' в игре '+GameName+' нашлась ошибка с кодом: '+ nameError+' Дата фиксации: '+date)
+        i[2] = 'Done'
+        i[3]= "0000-00-00"
+
+git
